bc4f85028a5bd11350c88a466601dc2745fd5084
Cezar Luca <cezar.luca@hotmail.com>
2024-08-25 20:25:48 +0200

    Updating Application Settings part 1

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiCabins";
+
+export function useUpdateSetting() {
+    const queryClient = useQueryClient();
+
+    const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+        mutationFn: updateSettingApi,
+        onSuccess: () => {
+            toast.success("Setting successfully edited");
+            queryClient.invalidateQueries({ queryKey: ["settings"] });
+            // reset();
+        },
+        onError: (error) => {
+            toast.error(
+                `An error occurred while editing the setting: ${error.message}`
+            );
+            console.error(error);
+        },
+    });
+
+    return { isUpdating, updateSetting };
+}
