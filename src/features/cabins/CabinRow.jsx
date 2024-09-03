4a01c5e734ccb4676ec853256c1422b5dedcc3af
Cezar Luca <cezar.luca@hotmail.com>
2024-09-03 17:49:25 +0200

    Confirming Cabin Deletions done

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,6 +6,7 @@ import { useDeleteCabin } from "./useDeleteCabins";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const TableRow = styled.div`
     display: grid;
@@ -96,13 +97,18 @@ function CabinRow({ cabin }) {
                     <Modal.Window name="edit">
                         <CreateCabinForm cabinToEdit={cabin} />
                     </Modal.Window>
-                    <button
-                        onClick={() => deleteCabin(cabinId)}
-                        disabled={isDeleting}
-                        role="cell"
-                    >
-                        <HiTrash />
-                    </button>
+                    <Modal.Open opens="delete">
+                        <button disabled={isDeleting} role="cell">
+                            <HiTrash />
+                        </button>
+                    </Modal.Open>
+                    <Modal.Window name="delete">
+                        <ConfirmDelete
+                            resourceName="cabins"
+                            disabled={isDeleting}
+                            onConfirm={() => deleteCabin(cabinId)}
+                        />
+                    </Modal.Window>
                 </Modal>
             </div>
         </TableRow>
