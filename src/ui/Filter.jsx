878c465927bd0cdacbabc73dce82741995395eff
Cezar Luca <cezar.luca@hotmail.com>
2024-09-08 21:42:06 +0200

    ClientSide Filtering Filtering Cabins done

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -38,6 +38,8 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
     const [searchParams, setSearchParams] = useSearchParams();
+    const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
     function handleClick(value) {
         searchParams.set(filterField, value);
         setSearchParams(searchParams);
@@ -49,6 +51,7 @@ function Filter({ filterField, options }) {
                 <FilterButton
                     key={option.value}
                     onClick={() => handleClick(option.value)}
+                    active={option.value === currentFilter}
                 >
                     {option.label}
                 </FilterButton>;
