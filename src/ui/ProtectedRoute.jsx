6416933eeb4bd61c970d4b5a2728114164377ec1
Cezar Luca <cezar.luca@hotmail.com>
2024-09-20 19:16:50 +0200

    Authorization Protecting Routes part 1

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,5 +1,11 @@
 function ProtectedRoute({ children }) {
-    // 1> Load the authenticated user
+    // 1. Load the authenticated user
+
+    // 2. While loading, show a loading spinner
+
+    // 3.If there is no authenticated user, redirect to the login page
+
+    // 4. If there is an authenticated user, show the children (render the app)
 
     return children;
 }
