0528ae90c0c1397683ad9f39dda9780e6c2baede
Cezar Luca <cezar.luca@hotmail.com>
2024-09-30 23:51:53 +0200

    Cleaned up DarkModeContext

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,10 +1,7 @@
-// import { createContext } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 import PropTypes from "prop-types";
 import { DarkModeContext } from "./useDarkMode";
 import { useEffect } from "react";
-
-// const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
     const [isDarkMode, setIsDarkMode] = useLocalStorageState(
@@ -33,20 +30,8 @@ function DarkModeProvider({ children }) {
     );
 }
 
-// function useDarkMode() {
-//     const context = useContext(DarkModeContext);
-//     if (!context) {
-//         throw new Error(
-//             "useDarkMode context must be used within a DarkModeProvider"
-//         );
-//     }
-//     return context;
-// }
-
 DarkModeProvider.propTypes = {
     children: PropTypes.node.isRequired,
 };
-
-// export { DarkModeProvider, useDarkMode };
 
 export default DarkModeProvider;
